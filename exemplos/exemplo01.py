f52e9036bd5edae97b578a2eaa614391488eb4f0
Fernando Matos <fernandommatos@outlook.com>
2025-01-13 21:51:21 -0300

    exemplo de ETL noSQL no Render

--- exemplos/exemplo01.py
+++ exemplos/exemplo01.py
@@ -1,6 +1,10 @@
 import requests
 
-url = "https://www.google.com/"
+url = "https://api.coinbase.com/v2/prices/spot"
+headers = {
+    "Accept": "application.json",
+    
+}
 
 resposta = requests.get(url)
 
